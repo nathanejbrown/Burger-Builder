727bd3f80ed880f06e2818fad58a5780039dad4e
Heathcliff <dennis.nathan@gmail.com>
2018-07-25 17:32:51 -0600

    Added logic in Burger.js to take in ingredients as props and loop through them, then render each on the DOM.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
   render() {
     return(
       <React.Fragment>
-        <div>Burger</div>
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients}/>
       </React.Fragment>
     );
   }
