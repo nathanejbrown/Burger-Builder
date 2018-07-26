0f25f5f8688d208ce52c656c87cdaa7fc167cbd2
Heathcliff <dennis.nathan@gmail.com>
2018-07-26 11:36:02 -0600

    Price of burger is now displayed and updated based on user interaction.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,10 +6,12 @@ const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.label}>{props.label}</div>
     <button
-      className={classes.Less} 
+      className={classes.Less}
       onClick={props.removed}
       disabled={props.disabled}>Less</button>
-    <button className={classes.More} onClick={props.added}>More</button>
+    <button
+      className={classes.More}
+      onClick={props.added}>More</button>
   </div>
 );
 
