9cbff59a447eb19fd6b03f3e59341e1bf0212ba2
Heathcliff <dennis.nathan@gmail.com>
2018-07-26 11:13:50 -0600

    Added addIngredientHandler in BurgerBuilder.js and passed it down to BuildControls and then BuildControl, allowing the user to add ingredients. This also updates a price variable in state, but that amount is not yet being accessed to display to the user.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  salad: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
 
@@ -10,12 +18,36 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
   }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice
+    })
+  }
+
+  removeIngredientHandler = (type) => {
+
+  }
+
   render() {
     return(
       <React.Fragment>
         <Burger ingredients={this.state.ingredients}/>
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}/>
       </React.Fragment>
     );
   }
