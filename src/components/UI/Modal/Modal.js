ca1dac3ac38324818f28f364c75bcbc1c2170aa2
Heathcliff <dennis.nathan@gmail.com>
2018-07-30 12:04:15 -0600

    Updated Ordersummary and Modal components to optimize app by not triggering unneccesary re renders.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <React.Fragment>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </React.Fragment>
-);
+class Modal extends Component {
+  //This component could be functional.
 
-export default modal;
+  //This stops OrderSummary, which is a child of Modal, from updating uneccessarily. It only updates when the 'show' prop changes.
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Modal;
