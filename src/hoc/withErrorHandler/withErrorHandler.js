8f97edb911182fa85531950e367f7c09ca0fa4fd
Heathcliff <dennis.nathan@gmail.com>
2018-07-31 17:36:15 -0600

    Added logic in withErrorHandler that ejects axios interceptors when the component unmounts.

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,14 +9,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
     }
 
     componentWillMount () {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       })
       // the res => res syntax simply implies return res; because error is the second argument that the function takes.
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({ error: error });
       });
+    }
+
+    componentWillUnmount () {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
