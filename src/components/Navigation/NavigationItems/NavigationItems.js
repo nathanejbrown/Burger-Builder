211e2cbb1ab1e7a953cfc766f6b2fff010a5d5f8
Heathcliff <dennis.nathan@gmail.com>
2018-08-04 13:10:39 -0600

    Got orders page fully functioning. It now accesses the database and gets all of my orders, then displays them.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/">Burger Builder</NavigationItem>
-    <NavigationItem link="/Burger-Builder">Checkout</NavigationItem>
+    <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+    <NavigationItem link="/Orders">Orders</NavigationItem>
   </ul>
 );
 
