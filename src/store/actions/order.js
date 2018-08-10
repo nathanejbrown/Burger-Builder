4af6e342837c2be1689adcfc09da812af11bd297
Heathcliff <dennis.nathan@gmail.com>
2018-08-10 15:53:09 -0600

    Fixed issue where user couldn't order a second burger after ordering the first.

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -27,11 +27,16 @@ export const purchaseBurger = (orderData) => {
     dispatch(purchaseBurgerStart());
     axios.post('/orders.json', orderData)
       .then(response => {
-        console.log(response)
         dispatch(purchaseBurgerSuccess(response.data.name, orderData))
       })
       .catch(error => {
         dispatch(purchaseBurgerFail(error))
       });
+  }
+}
+
+export const purchaseInit = () => {
+  return {
+    type: actionTypes.PURCHASE_INIT
   }
 }
